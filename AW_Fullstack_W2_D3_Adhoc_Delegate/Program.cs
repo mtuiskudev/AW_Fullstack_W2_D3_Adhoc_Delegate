00b7747871292ed7c3ccc73fded97db180507bc5
Martti Tuisku <martti.tuisku@sherpas.se>
2022-06-15 08:35:00 +0200

    Add project files.

---
+++ AW_Fullstack_W2_D3_Adhoc_Delegate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AW_Fullstack_W2_D3_Adhoc_Delegate
+{
+    class Program
+    {
+        public delegate void tulostaJotain(string jono);
+
+        public static void KirjoitaKonsoliin(string jono)
+        {
+            Console.WriteLine(jono);
+        }
+
+        public static void KirjoitaTakaperin(string jono)
+        {
+            char[] kirjainArray = jono.ToCharArray();
+            Array.Reverse(kirjainArray);
+            Console.WriteLine(new string(kirjainArray));
+        }
+
+        public static void kutsuDelegatea(tulostaJotain metodi)
+        {
+            metodi("Testi merkki jono");
+        }
+
+        static void Main(string[] args)
+        {
+            tulostaJotain eka = new tulostaJotain(KirjoitaKonsoliin);
+            tulostaJotain toka = new tulostaJotain(KirjoitaTakaperin);
+            kutsuDelegatea(eka);
+            kutsuDelegatea(toka);
+            
+        }
+    }
+}
